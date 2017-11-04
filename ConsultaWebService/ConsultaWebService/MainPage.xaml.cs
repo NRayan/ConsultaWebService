eb8004669f912375aa02b62b53c36054711c8951
Norton Rayan Meira de Oliveira <norton_rayan@hotmail.com>
2017-11-04 19:34:09 -0200

    V 0.01

--- ConsultaWebService/ConsultaWebService/MainPage.xaml.cs
+++ ConsultaWebService/ConsultaWebService/MainPage.xaml.cs
@@ -4,14 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using ConsultaWebService.service;
+using ConsultaWebService.models;
 
 namespace ConsultaWebService
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
+        DataService DAL;
+
+        public MainPage()
 		{
 			InitializeComponent();
+            DAL = new DataService();
 		}
-	}
+
+        private void btncarregar_Clicked(object sender, EventArgs e)
+        {
+            buscarCep();
+        }
+
+        async void buscarCep()
+        {
+            endereco end;
+            end = await DAL.buscarendereco(txtcep1.Text);
+            lblCEP.Text = "CEP: "+end.cep;
+            lblLog.Text = "Logradouro: "+end.logradouro;
+            lblbairro.Text = "Bairro: "+end.bairro;
+            lbllocalidade.Text = "Localidade: "+end.localidade;
+            lblUF.Text = "UF: "+end.uf;
+        }
+       
+    }
 }
---
+++ ConsultaWebService/ConsultaWebService/service/DataService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using ConsultaWebService.models;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsultaWebService.service
+{
+    public class DataService
+    {
+        HttpClient client = new HttpClient();
+              
+        public async Task<endereco> buscarendereco(string cep)
+        {
+            try
+            {
+                string url = "https://viacep.com.br/ws/"+cep+"/json/";
+                var resposta = await client.GetStringAsync(url);
+                var endr = JsonConvert.DeserializeObject<endereco>(resposta);
+                return endr;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        
+    }
+}
